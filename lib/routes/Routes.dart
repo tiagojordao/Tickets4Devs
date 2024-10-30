86e4dc7576493f13b2613ac6036ec8433b7e8c50
André Luis Gurgel de Faria Junior <50457552+andrejr95@users.noreply.github.com>
2024-10-29 22:26:41 -0300

    Add files via upload
    adicionando tela de perfil e criar conta

--- lib/routes/Routes.dart
+++ lib/routes/Routes.dart
@@ -1,13 +1,21 @@
-import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tickets4devs/screens/EventsAvailable.dart';
 import 'package:tickets4devs/screens/LoginScreen.dart';
+import 'package:tickets4devs/screens/SignUpScreen.dart';
+import 'package:tickets4devs/screens/UserProfileScreen.dart';
 
 final GoRouter myRouter = GoRouter(routes: <RouteBase>[
   GoRoute(
     path: '/',
     builder: (BuildContext context, GoRouterState state) {
-      return LoginScreen(); /*alterar depois para inicial */
+      return LoginScreen(); // Tela inicial, pode ser alterada para uma tela de boas-vindas
+    },
+  ),
+  GoRoute(
+    path: '/signup',
+    builder: (BuildContext context, GoRouterState state) {
+      return SignUpScreen();
     },
   ),
   GoRoute(
@@ -20,6 +28,15 @@ final GoRouter myRouter = GoRouter(routes: <RouteBase>[
     path: '/login',
     builder: (BuildContext context, GoRouterState state) {
       return LoginScreen();
+    },
+  ),
+  GoRoute(
+    path: '/profile',
+    builder: (BuildContext context, GoRouterState state) {
+      return UserProfileScreen(
+        name: "Nome do Usuário Teste",
+        email: "usuarioteste@email.com",
+      );
     },
   ),
 ]);
