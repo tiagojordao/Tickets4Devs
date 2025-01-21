9c7a5c0194d2d35914459a85852146c01ee654a1
gloria-mariass <gloria.souza.110@ufrn.edu.br>
2025-01-21 19:34:00 -0300

    Add geolocation and geocoding support with LocationNotifier

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -5,13 +5,12 @@ import 'package:provider/provider.dart';
 import 'package:tickets4devs/notifiers/Cart.dart';
 import 'package:tickets4devs/models/Event.dart';
 import 'package:tickets4devs/notifiers/UserNotifier.dart';
+import 'package:tickets4devs/notifiers/LocationNotifier.dart';
 import 'package:tickets4devs/widgets/BottomNavBar.dart';
 import 'package:http/http.dart' as http;
 import 'package:tickets4devs/widgets/EventCard.dart';
 
 class HomeScreen extends StatefulWidget {
-
-
   const HomeScreen({super.key});
 
   @override
@@ -19,7 +18,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   final int _selectedIndex = 0;
   List<Event> events = [];
   String? errorMessage;
@@ -31,8 +29,12 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     _fetchEvents();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final locationNotifier =
+          Provider.of<LocationNotifier>(context, listen: false);
+      locationNotifier.fetchLocation();
+    });
   }
-
 
   Future<void> _fetchEvents() async {
     try {
@@ -79,12 +81,15 @@ class _HomeScreenState extends State<HomeScreen> {
       if (purchasedEventIds.contains(eventId)) {
         purchasedEventIds.remove(eventId);
         //print(purchasedEventIds);
-        Provider.of<Cart>(context, listen: false)
-            .removeItemFromCartById(Provider.of<UserNotifier>(context, listen: false).userLogado, eventId);
+        Provider.of<Cart>(context, listen: false).removeItemFromCartById(
+            Provider.of<UserNotifier>(context, listen: false).userLogado,
+            eventId);
       } else {
         purchasedEventIds.add(eventId);
         //print(purchasedEventIds);
-        Provider.of<Cart>(context, listen: false).addItemFromCartById(Provider.of<UserNotifier>(context, listen: false).userLogado, eventId);
+        Provider.of<Cart>(context, listen: false).addItemFromCartById(
+            Provider.of<UserNotifier>(context, listen: false).userLogado,
+            eventId);
       }
       //print('Eventos comprados: $purchasedEventIds');
     });
@@ -98,132 +103,164 @@ class _HomeScreenState extends State<HomeScreen> {
       'https://media.istockphoto.com/id/501387734/pt/foto/dan%C3%A7ar-seus-amigos.jpg?s=612x612&w=0&k=20&c=aHB1gZ5Qm6Glsl0n4RGR4oI8r_nxe5QI06TYlw9ifdg=',
     ];
 
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(60.0),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: const BorderRadius.vertical(
-                  bottom: Radius.circular(30.0),
-                ),
-              ),
-              child: AppBar(
-                title: const Center(
-                  child: Text(
-                    'Tickets4Devs',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+    return Consumer<LocationNotifier>(
+      builder: (context, locationNotifier, child) {
+        // Pegue a cidade da localização diretamente do LocationNotifier
+        String? currentLocation = locationNotifier.currentCity;
+
+        if (locationNotifier.isLoading) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        print("current city: $currentLocation");
+
+        List<Event> filteredEvents = events.where((event) {
+          final eventLocation = event.localId;
+
+          // Separar o local em duas partes: antes e depois da vírgula
+          final parts = eventLocation.split(',');
+
+          // Verificar se existe uma cidade depois da vírgula
+          if (parts.length > 1) {
+            final eventCity = parts[1].trim(); // A cidade estará após a vírgula
+            print("oi $eventCity");
+            return eventCity ==
+                currentLocation; // Comparar com a cidade desejada
+          }
+
+          return false; // Caso não tenha cidade, retornamos false
+        }).toList();
+
+        return Scaffold(
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(60.0),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: const BorderRadius.vertical(
+                      bottom: Radius.circular(30.0),
                     ),
                   ),
-                ),
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-              ),
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-              color: Colors.black,
-            ),
-          ],
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 36,),
-              const Text(
-                'Bem-vindo ao Tickets4Devs!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 32),
-              CarouselSlider(
-                options: CarouselOptions(
-                  height: 200,
-                  autoPlay: true,
-                  enlargeCenterPage: true,
-                ),
-                items: carouselImages.map((imageUrl) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          image: DecorationImage(
-                            image: NetworkImage(imageUrl),
-                            fit: BoxFit.cover,
-                          ),
+                  child: AppBar(
+                    title: const Center(
+                      child: Text(
+                        'Tickets4Devs',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
                         ),
+                      ),
+                    ),
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                  ),
+                ),
+                const Divider(
+                  height: 1,
+                  thickness: 1,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 36),
+                  const Text(
+                    'Bem-vindo ao Tickets4Devs!',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  CarouselSlider(
+                    options: CarouselOptions(
+                      height: 200,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                    ),
+                    items: carouselImages.map((imageUrl) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              image: DecorationImage(
+                                image: NetworkImage(imageUrl),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }).toList(),
+                  ),
+                  const SizedBox(height: 44),
+                  const Text(
+                    'Eventos próximos a você',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ListView.builder(
+                    itemCount: filteredEvents.length,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      final event = filteredEvents[index];
+                      bool isPurchased = purchasedEventIds.contains(event.id);
+                      return EventCard(
+                        id: event.id,
+                        date: event.date,
+                        price: event.price,
+                        title: event.title,
+                        localId: event.localId,
+                        descricao: event.description,
+                        isPurchased: isPurchased,
+                        creator: event.criador,
+                        user: Provider.of<UserNotifier>(context, listen: false)
+                            .userLogado,
+                        togglePurchase: (eventid) {
+                          _togglePurchase(eventid);
+                        },
+                        onEventDeleted: _fetchEvents,
+                        shouldBuy: false,
                       );
                     },
-                  );
-                }).toList(),
+                  ),
+                ],
               ),
-              const SizedBox(height: 44),
-              const Text(
-                'Eventos próximos a você',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+            ),
+          ),
+          bottomNavigationBar: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Divider(
+                height: 4,
+                thickness: 2,
+                color: Colors.black,
               ),
-              const SizedBox(height: 20),
-              ListView.builder(
-                itemCount: events.length,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  final event = events[index];
-                  isPurchased = purchasedEventIds.contains(event.id);
-                  return EventCard(
-                    id: event.id,
-                    date: event.date,
-                    price: event.price,
-                    title: event.title,
-                    localId: event.localId,
-                    descricao: event.description,
-                    isPurchased: isPurchased,
-                    creator: event.criador,
-                    user: Provider.of<UserNotifier>(context, listen: false).userLogado,
-                    togglePurchase: (eventid) {
-                      _togglePurchase(eventid);
-                    },
-                    onEventDeleted: _fetchEvents,
-                    shouldBuy: false,
-                  );
-                },
+              BottomNavBar(
+                selectedIndex: _selectedIndex,
               ),
             ],
           ),
-        ),
-      ),
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Divider(
-            height: 4,
-            thickness: 2,
-            color: Colors.black,
-          ),
-          BottomNavBar(
-            selectedIndex: _selectedIndex,
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
